5fc209102889ac062f12478110cf2bc01a13460a
Natya Vidhan Biswas <67009562+studiousgamer@users.noreply.github.com>
2022-07-29 19:51:21 +0530

    Time: 66 ms (90.41%), Space: 13.9 MB (92.90%)

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,14 +1,11 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        n = len(s)
-        res = 0
-        for i in range(n):
-            visited = [0] * 256
-            for j in range(i, n):
-                if (visited[ord(s[j])] == True):
-                    break
-                else:
-                    res = max(res, j - i + 1)
-                    visited[ord(s[j])] = True
-            visited[ord(s[i])] = False
-        return res
+        last_idx = {}
+        max_len = 0
+        start_idx = 0
+        for i in range(0, len(s)):
+            if s[i] in last_idx:
+                start_idx = max(start_idx, last_idx[s[i]] + 1)
+            max_len = max(max_len, i-start_idx + 1)
+            last_idx[s[i]] = i
+        return max_len
