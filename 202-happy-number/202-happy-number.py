8824705c5f1b83f78505264d06291edc21612217
Natya Vidhan Biswas <67009562+studiousgamer@users.noreply.github.com>
2022-07-29 19:16:00 +0530

    Time: 68 ms (18.49%), Space: 13.8 MB (60.10%)

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -1,19 +1,9 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        slow = n
-        fast = n
-        while(True):
-            slow = self.numSquareSum(slow)
-            fast = self.numSquareSum(self.numSquareSum(fast))
-            if(slow != fast):
-                continue
-            else:
-                break
-        return (slow == 1)
-            
-    def numSquareSum(self, n):
-        squareSum = 0
-        while(n):
-            squareSum += (n % 10) * (n % 10)
-            n = int(n / 10)
-        return squareSum
+        past = set()
+        while n != 1:
+            n = sum(int(i)**2 for i in str(n))
+            if n in past:
+                return False
+            past.add(n)
+        return True
