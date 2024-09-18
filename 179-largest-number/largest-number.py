067ce68c3d5c5a2d7808466d58fdd8b8e5b37d73
Natya Vidhan Biswas <67009562+natyavidhan@users.noreply.github.com>
2024-09-18 10:00:19 +0530

    Time: 117 ms (5.31%) | Memory: 16.9 MB (32.99%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -3,10 +3,11 @@ class Solution:
         if not any(map(bool, nums)):
             return "0"
         nums = [str(i) for i in nums]
+        l = len(nums)
         for x, n in enumerate(nums):
-            if len(nums) > x+1:
-                for y in range(x+1, len(nums)):
-                    print(x, y, len(nums))
+            if l > x+1:
+                for y in range(x+1, l):
+                    print(x, y, l)
                     if int(nums[y]+nums[x]) > int(nums[x]+nums[y]):
                         nums[x], nums[y] = nums[y], nums[x]
         return "".join(nums)
